1143e69ecad0da80f6041d5cf9030274fef5067e
lincolndiogo200 <lincolndiogo200@gmail.com>
2024-07-24 11:38:48 -0400

    chore : adiciona wrapper da aplicacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-
-import './App.css'
-
 function App() {
-
   return (
-    <>
-      <main>
+    <main className="h-screen w-full bg-gradient-to-r from-[#66DECE] to-[#188982] flex items-center justify-center p-12">
+      <div className="bg-white w-full h-full rounded-3xl border backdrop-blur-sm bg-white/30">
         <h1>Seja bem vindo! :)</h1>
         <p>Desafio de apresentação Pessoal do Projeto Frontend fusion</p>
-      </main>
-    </>
+      </div>
+    </main>
   );
 }
 
-export default App
+export default App;
